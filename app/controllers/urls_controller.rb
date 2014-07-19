7744b144c9949a47887d6c58a1ade0b13198bde8
Alina Solovjova <alina@gocardless.com>
2014-07-19 13:56:01 +0100

    fixed show controller, added view

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,15 +1,27 @@
 class UrlsController < ApplicationController
 
 	def show
-
+		@url = Url.find(params[:id])
 	end
 
 	def new
-
+		@url = Url.new #new instance or Url model, and by default will create a new view
 	end
 
 	def create
-
+		@url = Url.new(safe_url_params)	#params is hash that contains the url, and inside the url is another hash which represents the model. 
+										#Inside that is the link "http://..."
+		@url.save
+		redirect_to @url
 	end
 
+	private
+
+		def safe_url_params
+			params.require('url').permit(:link)
+		end
+
+		def generate_hash_code
+			rand(10000)
+		end
 end
